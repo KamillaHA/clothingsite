6e7e70e3778939efa65d82f9b12255aa62b066ca
Kamilla Huhnke <kaan0001@stud.kea.dk>
2023-08-28 19:35:57 +0200

    categories

---
+++ js/category.js
@@ -0,0 +1,17 @@
+fetch("http://kea-alt-del.dk/t7/api/categories")
+  .then((res) => res.json())
+  .then(showCategories);
+
+function showCategories(cats) {
+  cats.forEach(showCategory);
+}
+
+function showCategory(cat) {
+  const template = document.querySelector("template").content;
+  const clone = template.cloneNode(true);
+
+  clone.querySelector("a").textContent = cat.category;
+  clone.querySelector("a").href = `productlist.html?category=${cat.category}`;
+
+  document.querySelector("letterGroup ul").appendChild(clone);
+}
